584897d696be61656cd13947d5515c958fc6ac1c
Javier <2190271@my.ipleiria.pt>
2020-06-08 17:19:50 +0100

    Move and throw ball

---
+++ Assets/MainBall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBall : MonoBehaviour
+{
+    private Rigidbody2D ballRigidbody;
+    [SerializeField]
+    private float speed = 3.5f;
+
+    private bool wasBallThrown = false;
+
+    private void Awake()
+    {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (!wasBallThrown)
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                ThrowBall();
+            }
+
+
+            ballRigidbody.velocity =
+                  new Vector2(horizontalInput * speed,
+                  ballRigidbody.velocity.y
+                  );
+        }
+    }
+
+    private void ThrowBall()
+    {
+        ballRigidbody.gravityScale = 1;
+        wasBallThrown = true;
+    }
+}
