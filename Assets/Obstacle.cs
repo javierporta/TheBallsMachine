6b8cbf0c19f971594149545b863e7e90d23757e1
Javier <2190271@my.ipleiria.pt>
2020-06-11 15:01:56 +0100

    Add obstacle script and changed prefabs to use it

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField]
+    private int obstacleLife = 5;
+
+    [SerializeField]
+    private Text lifeText;
+
+    private void Awake()
+    {
+        lifeText.text = obstacleLife.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ball"))
+        {
+            if (obstacleLife > 1)
+            {
+                //TODO: Play sound
+                obstacleLife--;
+                lifeText.text = obstacleLife.ToString();
+            }
+            else
+                //ToDo: animation, explotion, sound, sum extra score!
+                //dissapear
+                Destroy(gameObject);
+
+            }
+            
+
+        }
+    }
+}
