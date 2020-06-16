c70c0b87395103323030c54a3cd0c5f1b4a9ac2d
Javier <2190271@my.ipleiria.pt>
2020-06-16 15:10:36 +0100

    First approach to spawning balls

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -14,6 +14,15 @@ public class Obstacle : MonoBehaviour
     [SerializeField]
     private Text lifeText;
 
+    [SerializeField]
+    private int ballsToSpawn = 3;
+
+    [SerializeField]
+    private float spawnedBallSpeed = 5f;
+
+    [SerializeField]
+    private GameObject ballPrefab;
+
     private void Awake()
     {
         lifeText.text = obstacleLife.ToString();
@@ -21,11 +30,20 @@ public class Obstacle : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Ball"))
+        if (col.gameObject.CompareTag("MainBall"))
+        {
+            if (obstacleLife > 0)
+            {
+                SpawnBalls();
+            }
+        }
+
+        if (col.gameObject.CompareTag("Ball") || col.gameObject.CompareTag("MainBall"))
         {
             if (obstacleLife > 1)
             {
                 //TODO: Play sound
+
                 obstacleLife--;
                 lifeText.text = obstacleLife.ToString();
             }
@@ -39,5 +57,11 @@ public class Obstacle : MonoBehaviour
 
 
         }
+    }
+
+    private void SpawnBalls()
+    {
+        GameObject ball = Instantiate(ballPrefab, transform.position, transform.rotation);
+        ball.GetComponent<Rigidbody2D>().velocity = (transform.right) * spawnedBallSpeed;
     }
 }
