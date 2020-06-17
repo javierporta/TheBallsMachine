3208973539453ae8b3e8c6ae11b53e6d4c046e2b
Javier <2190271@my.ipleiria.pt>
2020-06-17 14:36:45 +0100

    Add UI canvas

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; } = null;
+
+    [SerializeField]
+    private Text scoreText;
+
+    [SerializeField]
+    private Text currentLevelText;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void UpdateScore(int score)
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void UpdateCurrentLevel(int currentLevel)
+    {
+        currentLevelText.text = currentLevel.ToString();
+    }
+}
