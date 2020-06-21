e2eb8389eb20e8d32cfe057867c7873d02c5b4d1
royli <roylihernandez@gmail.com>
2020-06-21 13:35:58 +0100

    add robotic_arm script

---
+++ Assets/Scripts/RoboticArm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RoboticArm : MonoBehaviour
+{
+
+    private Animator myAnimator;
+
+    private bool idOpen = false;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+
+        if (CrossPlatformInputManager.GetButtonDown("Fire1") && !idOpen)
+        {
+            myAnimator.SetTrigger("open_arm");
+            idOpen = true;
+        }
+    }
+}
